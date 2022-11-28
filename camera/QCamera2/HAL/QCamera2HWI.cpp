ProtoDump/device_xiaomi_santoni

commit c1c24df08b46133e8e07ab24b7930f3ba781cf09

santoni: camera: Support Xiaomi makeup parameters
Change-Id: I1174266a3da15b474ee62d1ed100596ef55753c6

camera/QCamera2/HAL/QCameraParameters.cpp
@@ -492,6 +492,9 @@ const char QCameraParameters::KEY_QC_NOISE_REDUCTION_MODE_VALUES[] = "noise-redu
 const char QCameraParameters::KEY_TS_MAKEUP[] = "tsmakeup";
 const char QCameraParameters::KEY_TS_MAKEUP_WHITEN[] = "tsmakeup_whiten";
 const char QCameraParameters::KEY_TS_MAKEUP_CLEAN[] = "tsmakeup_clean";
+const char QCameraParameters::KEY_XM_MAKEUP[] = "xiaomi-still-beautify-values";
+const char QCameraParameters::KEY_XM_MAKEUP_WHITEN[] = "xiaomi-beauty-skin-color";
+const char QCameraParameters::KEY_XM_MAKEUP_CLEAN[] = "xiaomi-beauty-skin-smooth";
 #endif
 
 //KEY to share HFR batch size with video encoder.
@@ -4178,6 +4181,59 @@ int32_t QCameraParameters::setTsMakeup(const QCameraParameters& params)
     return NO_ERROR;
 }
 
+/*===========================================================================
+ * FUNCTION   : setXmMakeup
+ *
+ * DESCRIPTION: set setXmMakeup from user setting
+ *
+ * PARAMETERS :
+ *   @params  : user setting parameters
+ *
+ * RETURN     : int32_t type of status
+ *              NO_ERROR  -- success
+ *              none-zero failure code
+ *==========================================================================*/
+int32_t QCameraParameters::setXmMakeup(const QCameraParameters& params)
+{
+    const char *str = params.get(KEY_XM_MAKEUP);
+    const char *prev_str = get(KEY_XM_MAKEUP);
+
+    LOGH("str =%s & prev_str =%s", str, prev_str);
+
+    if (str != NULL) {
+        if (prev_str == NULL) {
+            m_bNeedRestart = true;
+        } else if (strcmp(str, prev_str) != 0) {
+            const char* prev_enabled = strrchr(prev_str, ':');
+            const char* curr_enabled = strrchr(str, ':');
+            if (prev_enabled && curr_enabled) {
+                prev_enabled++;
+                curr_enabled++;
+                m_bNeedRestart = (atoi(prev_enabled) > 0 != atoi(curr_enabled) > 0) ?
+                        true : m_bNeedRestart;
+            }
+            set(KEY_XM_MAKEUP, str);
+        }
+
+        str = params.get(KEY_XM_MAKEUP_WHITEN);
+        prev_str = get(KEY_XM_MAKEUP_WHITEN);
+        if (str != NULL) {
+            if (prev_str == NULL || strcmp(str, prev_str) != 0) {
+                set(KEY_XM_MAKEUP_WHITEN, str);
+            }
+        }
+
+        str = params.get(KEY_XM_MAKEUP_CLEAN);
+        prev_str = get(KEY_XM_MAKEUP_CLEAN);
+        if (str != NULL) {
+            if (prev_str == NULL || strcmp(str, prev_str) != 0) {
+                set(KEY_XM_MAKEUP_CLEAN, str);
+            }
+        }
+    }
+
+    return NO_ERROR;
+}
 #endif
 
 /*===========================================================================
@@ -5298,6 +5354,7 @@ int32_t QCameraParameters::updateParameters(const String8& p,
     if ((rc = updateFlash(false)))                      final_rc = rc;
 #ifdef TARGET_TS_MAKEUP
     if ((rc = setTsMakeup(params)))                     final_rc = rc;
+    if ((rc = setXmMakeup(params)))                     final_rc = rc;
 #endif
     if ((rc = setAdvancedCaptureMode()))                final_rc = rc;
 UPDATE_PARAM_DONE:
@@ -10509,15 +10566,29 @@ char* QCameraParameters::getParameters()
 bool QCameraParameters::getTsMakeupInfo(int &whiteLevel, int &cleanLevel) const
 {
     const char* pch_makeup_enable = get(QCameraParameters::KEY_TS_MAKEUP);
-    if (pch_makeup_enable == NULL) {
-        LOGH("pch_makeup_enable = null");
-        return false;
-    }
-    bool enableMakeup =
-            (strcmp(pch_makeup_enable,"On") == 0);
-    if (enableMakeup) {
-        whiteLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_WHITEN);
-        cleanLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_CLEAN);
+    bool enableMakeup = false;
+    if (pch_makeup_enable != NULL) {
+        enableMakeup =
+                (strcmp(pch_makeup_enable,"On") == 0);
+        if (enableMakeup) {
+            whiteLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_WHITEN);
+            cleanLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_CLEAN);
+        }
+    } else {
+        pch_makeup_enable = get(QCameraParameters::KEY_XM_MAKEUP);
+        if (pch_makeup_enable != NULL) {
+            const char* xm_makeup_enable = strrchr(pch_makeup_enable, ':');
+            if (xm_makeup_enable == NULL) {
+                return false;
+            }
+            xm_makeup_enable++;
+            enableMakeup = atoi(xm_makeup_enable) > 0;
+            LOGD("Xiaomi makeup enable %d", (int)enableMakeup);
+            if (enableMakeup) {
+                whiteLevel = getInt(QCameraParameters::KEY_XM_MAKEUP_WHITEN) * 10;
+                cleanLevel = getInt(QCameraParameters::KEY_XM_MAKEUP_CLEAN) * 10;
+            }
+        }
     }
     return enableMakeup;
 }

camera/QCamera2/HAL/QCameraParameters.h
@@ -615,6 +615,9 @@ private:
     static const char KEY_TS_MAKEUP[];
     static const char KEY_TS_MAKEUP_WHITEN[];
     static const char KEY_TS_MAKEUP_CLEAN[];
+    static const char KEY_XM_MAKEUP[];
+    static const char KEY_XM_MAKEUP_WHITEN[];
+    static const char KEY_XM_MAKEUP_CLEAN[];
 #endif
     //param key for HFR batch size
     static const char KEY_QC_VIDEO_BATCH_SIZE[];
@@ -954,6 +957,7 @@ private:
     int32_t setStillMore(const QCameraParameters& );
 #ifdef TARGET_TS_MAKEUP
     int32_t setTsMakeup(const QCameraParameters& );
+    int32_t setXmMakeup(const QCameraParameters& );
 #endif
     int32_t setNoiseReductionMode(const QCameraParameters& );
     int32_t setRedeyeReduction(const QCameraParameters& );

thebassraja/android_device_xiaomi_vince

commit cb571d7ee9e75a255df6fc5a1204cbdd520c60f5

vince: QCamera2: HAL: Fix memory corruption issue.
Issue:
Memory is being used after free which can
lead to memory corruption.
Fix:
Ensured memory is not being used after free.

Change-Id: Icf03660de6fbb6c2aa5cbfc766c062d970142d5b
(cherry-picked from commit a33465a2bde7d8d978f23433478a3406b3622268)

Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

mido: QCamera2: Enable antibanding to fix the flickers issue

The GMS Camera app does not have antibanding setting.
So set default antibanding setting as Auto.

Change-Id: Ia61e9623a6c180a1c2a972b6c16eb7b2277a2393
Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

mido: camera: Support Xiaomi makeup parameters

Change-Id: I1174266a3da15b474ee62d1ed100596ef55753c6

mido: camera: Set KEY_XM_MAKEUP when change in value is detected

Fixes: c1c24df08b4 ("santoni: camera: Support Xiaomi makeup parameters")

Change-Id: I8bfc85ecca62ded30aaae107c1d2f0f84359e020

QCamera2: HAL: Fix padding issue in livesnapshot - 480p_HFR60 & restart device in 1080P_HFR60

Issue :-
1)Snapshot picture display green stripe in livesnapshot 480p_HFR60/CIF mode.
Issue was happening if livesnapshot take in portrait mode (100%)

If rotation is there then Main/thumbnail reprocess dimensions are
swapped before giving to JPEG encode And stride and scanline
are not meeting the padding requirement

2)Camcorder app restart device while launch snapshot with 1080P_HFR60 mode. (100%)

Fix:
Reprocess output should have same padding in both directions
i,e, stride and scanline should have same padding
and it should be 64 as per padding requirement for CPP.

CRs-Fixed: 2202135
CRs-Fixed: 2203744

Change-Id: I7bf7b7cb28a680ff79cb4651b8d81132f012ab32
(cherry-picked from commit 11638e25d5a38bce0869fb76125244ce2687eb2b)

Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

QCamera2: Fix for incorrect min frame duration

Issue:
While calculating min frame duration raw min frame
duration is being considered even if raw stream is

camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -1680,12 +1680,10 @@ QCamera2HardwareInterface::QCamera2HardwareInterface(uint32_t cameraId)
     mCameraDevice.ops = &mCameraOps;
     mCameraDevice.priv = this;
 
-
     pthread_condattr_t mCondAttr;
 
     pthread_condattr_init(&mCondAttr);
     pthread_condattr_setclock(&mCondAttr, CLOCK_MONOTONIC);
-
 
     pthread_mutex_init(&m_lock, NULL);
     pthread_cond_init(&m_cond, &mCondAttr);

camera/QCamera2/HAL/QCameraParameters.cpp
@@ -491,6 +491,9 @@ const char QCameraParameters::KEY_QC_NOISE_REDUCTION_MODE_VALUES[] = "noise-redu
 const char QCameraParameters::KEY_TS_MAKEUP[] = "tsmakeup";
 const char QCameraParameters::KEY_TS_MAKEUP_WHITEN[] = "tsmakeup_whiten";
 const char QCameraParameters::KEY_TS_MAKEUP_CLEAN[] = "tsmakeup_clean";
+const char QCameraParameters::KEY_XM_MAKEUP[] = "xiaomi-still-beautify-values";
+const char QCameraParameters::KEY_XM_MAKEUP_WHITEN[] = "xiaomi-beauty-skin-color";
+const char QCameraParameters::KEY_XM_MAKEUP_CLEAN[] = "xiaomi-beauty-skin-smooth";
 #endif
 
 //KEY to share HFR batch size with video encoder.
@@ -4183,6 +4186,60 @@ int32_t QCameraParameters::setTsMakeup(const QCameraParameters& params)
     return NO_ERROR;
 }
 
+/*===========================================================================
+ * FUNCTION   : setXmMakeup
+ *
+ * DESCRIPTION: set setXmMakeup from user setting
+ *
+ * PARAMETERS :
+ *   @params  : user setting parameters
+ *
+ * RETURN     : int32_t type of status
+ *              NO_ERROR  -- success
+ *              none-zero failure code
+ *==========================================================================*/
+int32_t QCameraParameters::setXmMakeup(const QCameraParameters& params)
+{
+    const char *str = params.get(KEY_XM_MAKEUP);
+    const char *prev_str = get(KEY_XM_MAKEUP);
+
+    LOGH("str =%s & prev_str =%s", str, prev_str);
+
+    if (str != NULL) {
+        if (prev_str == NULL) {
+            m_bNeedRestart = true;
+            set(KEY_XM_MAKEUP, str);
+        } else if (strcmp(str, prev_str) != 0) {
+            const char* prev_enabled = strrchr(prev_str, ':');
+            const char* curr_enabled = strrchr(str, ':');
+            if (prev_enabled && curr_enabled) {
+                prev_enabled++;
+                curr_enabled++;
+                m_bNeedRestart = (atoi(prev_enabled) > 0 != atoi(curr_enabled) > 0) ?
+                        true : m_bNeedRestart;
+            }
+            set(KEY_XM_MAKEUP, str);
+        }
+
+        str = params.get(KEY_XM_MAKEUP_WHITEN);
+        prev_str = get(KEY_XM_MAKEUP_WHITEN);
+        if (str != NULL) {
+            if (prev_str == NULL || strcmp(str, prev_str) != 0) {
+                set(KEY_XM_MAKEUP_WHITEN, str);
+            }
+        }
+
+        str = params.get(KEY_XM_MAKEUP_CLEAN);
+        prev_str = get(KEY_XM_MAKEUP_CLEAN);
+        if (str != NULL) {
+            if (prev_str == NULL || strcmp(str, prev_str) != 0) {
+                set(KEY_XM_MAKEUP_CLEAN, str);
+            }
+        }
+    }
+
+    return NO_ERROR;
+}
 #endif
 
 /*===========================================================================
@@ -5302,6 +5359,7 @@ int32_t QCameraParameters::updateParameters(const String8& p,
     if ((rc = updateFlash(false)))                      final_rc = rc;
 #ifdef TARGET_TS_MAKEUP
     if ((rc = setTsMakeup(params)))                     final_rc = rc;
+    if ((rc = setXmMakeup(params)))                     final_rc = rc;
 #endif
     if ((rc = setAdvancedCaptureMode()))                final_rc = rc;
 UPDATE_PARAM_DONE:
@@ -5648,7 +5706,7 @@ int32_t QCameraParameters::initDefaultParameters()
             AUTO_EXPOSURE_MAP,
             PARAM_MAP_SIZE(AUTO_EXPOSURE_MAP));
     set(KEY_QC_SUPPORTED_AUTO_EXPOSURE, autoExposureValues.string());
-    setAutoExposure(AUTO_EXPOSURE_FRAME_AVG);
+    setAutoExposure(AUTO_EXPOSURE_CENTER_WEIGHTED);
 
     // Set Exposure Compensation
     set(KEY_MAX_EXPOSURE_COMPENSATION, m_pCapability->exposure_compensation_max); // 12
@@ -5680,7 +5738,7 @@ int32_t QCameraParameters::initDefaultParameters()
             ANTIBANDING_MODES_MAP,
             PARAM_MAP_SIZE(ANTIBANDING_MODES_MAP));
     set(KEY_SUPPORTED_ANTIBANDING, antibandingValues);
-    setAntibanding(ANTIBANDING_OFF);
+    setAntibanding(ANTIBANDING_AUTO);
 
     // Set Effect
     String8 effectValues = createValuesString(
@@ -10516,15 +10574,29 @@ char* QCameraParameters::getParameters()
 bool QCameraParameters::getTsMakeupInfo(int &whiteLevel, int &cleanLevel) const
 {
     const char* pch_makeup_enable = get(QCameraParameters::KEY_TS_MAKEUP);
-    if (pch_makeup_enable == NULL) {
-        LOGH("pch_makeup_enable = null");
-        return false;
-    }
-    bool enableMakeup =
-            (strcmp(pch_makeup_enable,"On") == 0);
-    if (enableMakeup) {
-        whiteLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_WHITEN);
-        cleanLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_CLEAN);
+    bool enableMakeup = false;
+    if (pch_makeup_enable != NULL) {
+        enableMakeup =
+                (strcmp(pch_makeup_enable,"On") == 0);
+        if (enableMakeup) {
+            whiteLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_WHITEN);
+            cleanLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_CLEAN);
+        }
+    } else {
+        pch_makeup_enable = get(QCameraParameters::KEY_XM_MAKEUP);
+        if (pch_makeup_enable != NULL) {
+            const char* xm_makeup_enable = strrchr(pch_makeup_enable, ':');
+            if (xm_makeup_enable == NULL) {
+                return false;
+            }
+            xm_makeup_enable++;
+            enableMakeup = atoi(xm_makeup_enable) > 0;
+            LOGD("Xiaomi makeup enable %d", (int)enableMakeup);
+            if (enableMakeup) {
+                whiteLevel = getInt(QCameraParameters::KEY_XM_MAKEUP_WHITEN) * 10;
+                cleanLevel = getInt(QCameraParameters::KEY_XM_MAKEUP_CLEAN) * 10;
+            }
+        }
     }
     return enableMakeup;
 }

camera/QCamera2/HAL/QCameraParameters.h
@@ -616,6 +616,9 @@ private:
     static const char KEY_TS_MAKEUP[];
     static const char KEY_TS_MAKEUP_WHITEN[];
     static const char KEY_TS_MAKEUP_CLEAN[];
+    static const char KEY_XM_MAKEUP[];
+    static const char KEY_XM_MAKEUP_WHITEN[];
+    static const char KEY_XM_MAKEUP_CLEAN[];
 #endif
     //param key for HFR batch size
     static const char KEY_QC_VIDEO_BATCH_SIZE[];
@@ -958,6 +961,7 @@ private:
     int32_t setStillMore(const QCameraParameters& );
 #ifdef TARGET_TS_MAKEUP
     int32_t setTsMakeup(const QCameraParameters& );
+    int32_t setXmMakeup(const QCameraParameters& );
 #endif
     int32_t setNoiseReductionMode(const QCameraParameters& );
     int32_t setRedeyeReduction(const QCameraParameters& );

camera/QCamera2/HAL/QCameraPostProc.cpp
@@ -1618,6 +1618,7 @@ void QCameraPostProcessor::releaseOngoingPPData(void *data, void *user_data)
                 && (pp_job->offline_buffer)) {
             free(pp_job->offline_reproc_buf);
             pp_job->offline_buffer = false;
+            pp_job->offline_reproc_buf = NULL;
         }
         pp_job->reprocCount = 0;
     }
@@ -3182,14 +3183,18 @@ int32_t QCameraPostProcessor::doReprocess()
                 ret = mPPChannels[mCurChannelIdx]->doReprocessOffline(ppInputFrame,
                         meta_buf, m_parent->mParameters);
                 if (ret != NO_ERROR) {
+                    m_ongoingPPQ.dequeue(false);
                     pthread_mutex_unlock(&m_reprocess_lock);
                     goto end;
                 }
 
                 if ((ppreq_job->offline_buffer) &&
                         (ppreq_job->offline_reproc_buf)) {
-                    mPPChannels[mCurChannelIdx]->doReprocessOffline(
+                    ret = mPPChannels[mCurChannelIdx]->doReprocessOffline(
                             ppreq_job->offline_reproc_buf, meta_buf);
+                    if (ret != NO_ERROR) {
+                        m_ongoingPPQ.dequeue(false);
+                    }
                 }
                 pthread_mutex_unlock(&m_reprocess_lock);
             } else {
@@ -3234,6 +3239,9 @@ int32_t QCameraPostProcessor::doReprocess()
 
             ret = mPPChannels[mCurChannelIdx]->doReprocess(ppInputFrame,
                     m_parent->mParameters, pMetaStream, meta_buf_index);
+            if (ret != NO_ERROR) {
+                m_ongoingPPQ.dequeue(false);
+            }
         }
     } else {
         LOGE("Reprocess channel is NULL");

camera/QCamera2/HAL/QCameraStateMachine.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -1323,9 +1323,9 @@ int32_t QCameraStateMachine::procEvtPreviewingState(qcamera_sm_evt_enum_t evt,
         break;
     case QCAMERA_SM_EVT_STOP_PREVIEW:
         {
+            m_state = QCAMERA_SM_STATE_PREVIEW_STOPPED;
             rc = m_parent->stopPreview();
             applyDelayedMsgs();
-            m_state = QCAMERA_SM_STATE_PREVIEW_STOPPED;
             result.status = rc;
             result.request_api = evt;
             result.result_type = QCAMERA_API_RESULT_TYPE_DEF;

camera/QCamera2/HAL/QCameraStream.cpp
@@ -820,8 +820,8 @@ int32_t QCameraStream::calcOffset(cam_stream_info_t *streamInfo)
                 &streamInfo->buf_planes);
         break;
     case CAM_STREAM_TYPE_VIDEO:
-        rc = mm_stream_calc_offset_video(streamInfo->fmt,
-                &dim, &streamInfo->buf_planes);
+        rc = mm_stream_calc_offset_video(streamInfo,
+                &mPaddingInfo, &streamInfo->buf_planes);
         break;
     case CAM_STREAM_TYPE_RAW:
         rc = mm_stream_calc_offset_raw(streamInfo->fmt,

camera/QCamera2/HAL3/QCamera3Channel.cpp
@@ -1560,8 +1560,8 @@ int32_t QCamera3ProcessingChannel::setReprocConfig(reprocess_config_t &reproc_cf
                     &reproc_cfg.input_stream_plane_info);
             break;
         case CAM_STREAM_TYPE_VIDEO:
-            rc = mm_stream_calc_offset_video(reproc_cfg.stream_format,
-                    &reproc_cfg.input_stream_dim,
+            rc = mm_stream_calc_offset_video(getStreamByIndex(0)->getStreamInfo(),
+                    reproc_cfg.padding,
                     &reproc_cfg.input_stream_plane_info);
             break;
         case CAM_STREAM_TYPE_RAW:

camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -1693,8 +1693,8 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
         return -EINVAL;
     }
     /* Check whether we have zsl stream or 4k video case */
-    if (isZsl && m_bIsVideo) {
-        LOGE("Currently invalid configuration ZSL&Video!");
+    if (isZsl && m_bIs4KVideo) {
+        LOGE("Currently invalid configuration ZSL & 4K Video!");
         pthread_mutex_unlock(&mMutex);
         return -EINVAL;
     }
@@ -3335,8 +3335,8 @@ void QCamera3HardwareInterface::restoreHdrScene(
 void QCamera3HardwareInterface::hdrPlusPerfLock(
         mm_camera_super_buf_t *metadata_buf)
 {
-    if (NULL == metadata_buf) {
-        LOGE("metadata_buf is NULL");
+    if ((NULL == metadata_buf) || (ERROR == mState)) {
+        LOGE("metadata_buf is NULL or return when mState is error");
         return;
     }
     metadata_buffer_t *metadata =
@@ -5372,8 +5372,8 @@ void QCamera3HardwareInterface::captureResultCb(mm_camera_super_buf_t *metadata_
             handleBatchMetadata(metadata_buf,
                     true /* free_and_bufdone_meta_buf */);
         } else { /* mBatchSize = 0 */
-            hdrPlusPerfLock(metadata_buf);
             pthread_mutex_lock(&mMutex);
+            hdrPlusPerfLock(metadata_buf);
             handleMetadataWithLock(metadata_buf,
                     true /* free_and_bufdone_meta_buf */,
                     false /* first frame of batch metadata */ );

camera/QCamera2/HAL3/QCamera3Stream.cpp
@@ -643,6 +643,7 @@ void *QCamera3Stream::dataProcRoutine(void *data)
                     } else {
                         // no data cb routine, return buf here
                         pme->bufDone(frame->bufs[0]->buf_idx);
+                        free(frame);
                     }
                 }
             }

camera/QCamera2/stack/common/mm_camera_interface.h
@@ -911,8 +911,8 @@ int32_t mm_stream_calc_offset_raw(cam_format_t fmt,
         cam_padding_info_t *padding,
         cam_stream_buf_plane_info_t *buf_planes);
 
-int32_t mm_stream_calc_offset_video(cam_format_t fmt,
-        cam_dimension_t *dim,
+int32_t mm_stream_calc_offset_video(cam_stream_info_t *stream_info,
+        cam_padding_info_t *padding,
         cam_stream_buf_plane_info_t *buf_planes);
 
 int32_t mm_stream_calc_offset_metadata(cam_dimension_t *dim,

camera/QCamera2/stack/mm-camera-interface/src/mm_camera_channel.c
@@ -2924,12 +2924,14 @@ int32_t mm_channel_superbuf_comp_and_enqueue(
                         }
                     }
                     queue->que.size--;
+                    last_buf_ptr = last_buf_ptr->next;
                     cam_list_del_node(&node->list);
                     free(node);
                     free(super_buf);
                     unmatched_bundles--;
+                } else {
+                    last_buf_ptr = last_buf_ptr->next;
                 }
-                last_buf_ptr = last_buf_ptr->next;
             }
 
             if (queue->attr.max_unmatched_frames < unmatched_bundles) {

camera/QCamera2/stack/mm-camera-interface/src/mm_camera_stream.c
@@ -99,8 +99,8 @@ int32_t mm_stream_calc_offset_raw(cam_format_t fmt,
                                   cam_dimension_t *dim,
                                   cam_padding_info_t *padding,
                                   cam_stream_buf_plane_info_t *buf_planes);
-int32_t mm_stream_calc_offset_video(cam_format_t fmt,
-        cam_dimension_t *dim,
+int32_t mm_stream_calc_offset_video(cam_stream_info_t *stream_info,
+        cam_padding_info_t *padding,
         cam_stream_buf_plane_info_t *buf_planes);
 int32_t mm_stream_calc_offset_metadata(cam_dimension_t *dim,
                                        cam_padding_info_t *padding,
@@ -3807,26 +3807,27 @@ int32_t mm_stream_calc_offset_raw(cam_format_t fmt,
  *              padding information
  *
  * PARAMETERS :
-  *   @fmt     : image format
- *   @dim     : image dimension
+  *   @stream_info  : Stream information
+ *   @padding : Padding info
  *   @buf_planes : [out] buffer plane information
  *
  * RETURN     : int32_t type of status
  *              0  -- success
  *              -1 -- failure
  *==========================================================================*/
-int32_t mm_stream_calc_offset_video(cam_format_t fmt,
-        cam_dimension_t *dim, cam_stream_buf_plane_info_t *buf_planes)
+int32_t mm_stream_calc_offset_video(cam_stream_info_t *stream_info,
+        cam_padding_info_t *padding, cam_stream_buf_plane_info_t *buf_planes)
 {
     int32_t rc = 0;
     int stride = 0, scanline = 0;
+    cam_dimension_t *dim = &stream_info->dim ;
 
     #ifdef UBWC_PRESENT
     int meta_stride = 0,meta_scanline = 0;
     #endif
 
 
-    switch (fmt) {
+    switch (stream_info->fmt) {
         case CAM_FORMAT_YUV_420_NV12:
         case CAM_FORMAT_Y_ONLY:
         case CAM_FORMAT_Y_ONLY_10_BPP:
@@ -3868,11 +3869,16 @@ int32_t mm_stream_calc_offset_video(cam_format_t fmt,
         case CAM_FORMAT_YUV_420_NV12_VENUS:
 #ifdef VENUS_PRESENT
             // using Venus
-            stride = VENUS_Y_STRIDE(COLOR_FMT_NV12, dim->width);
-            scanline = VENUS_Y_SCANLINES(COLOR_FMT_NV12, dim->height);
+            if (stream_info->stream_type != CAM_STREAM_TYPE_OFFLINE_PROC) {
+                stride = VENUS_Y_STRIDE(COLOR_FMT_NV12, dim->width);
+                scanline = VENUS_Y_SCANLINES(COLOR_FMT_NV12, dim->height);
+            } else {
+                stride = PAD_TO_SIZE(dim->width, padding->width_padding);
+                scanline = PAD_TO_SIZE(dim->height, padding->height_padding);
+            }
 
             buf_planes->plane_info.frame_len =
-                    VENUS_BUFFER_SIZE(COLOR_FMT_NV12, dim->width, dim->height);
+                    VENUS_BUFFER_SIZE(COLOR_FMT_NV12, stride, scanline);
             buf_planes->plane_info.num_planes = 2;
             buf_planes->plane_info.mp[0].len = (uint32_t)(stride * scanline);
             buf_planes->plane_info.mp[0].offset = 0;
@@ -3882,8 +3888,13 @@ int32_t mm_stream_calc_offset_video(cam_format_t fmt,
             buf_planes->plane_info.mp[0].scanline = scanline;
             buf_planes->plane_info.mp[0].width = dim->width;
             buf_planes->plane_info.mp[0].height = dim->height;
-            stride = VENUS_UV_STRIDE(COLOR_FMT_NV12, dim->width);
-            scanline = VENUS_UV_SCANLINES(COLOR_FMT_NV12, dim->height);
+            if (stream_info->stream_type != CAM_STREAM_TYPE_OFFLINE_PROC) {
+                stride = VENUS_UV_STRIDE(COLOR_FMT_NV12, dim->width);
+                scanline = VENUS_UV_SCANLINES(COLOR_FMT_NV12, dim->height);
+            } else {
+                stride = PAD_TO_SIZE(dim->width, padding->width_padding);
+                scanline = PAD_TO_SIZE(dim->height, padding->height_padding);
+            }
             buf_planes->plane_info.mp[1].len =
                     buf_planes->plane_info.frame_len -
                     buf_planes->plane_info.mp[0].len;
@@ -3896,17 +3907,22 @@ int32_t mm_stream_calc_offset_video(cam_format_t fmt,
             buf_planes->plane_info.mp[1].height = dim->height/2;
 #else
             LOGD("Video format VENUS is not supported = %d",
-                     fmt);
+                     stream_info->fmt);
 #endif
             break;
         case CAM_FORMAT_YUV_420_NV21_VENUS:
 #ifdef VENUS_PRESENT
             // using Venus
-            stride = VENUS_Y_STRIDE(COLOR_FMT_NV21, dim->width);
-            scanline = VENUS_Y_SCANLINES(COLOR_FMT_NV21, dim->height);
+            if (stream_info->stream_type != CAM_STREAM_TYPE_OFFLINE_PROC) {
+                stride = VENUS_Y_STRIDE(COLOR_FMT_NV21, dim->width);
+                scanline = VENUS_Y_SCANLINES(COLOR_FMT_NV21, dim->height);
+            } else {
+                stride = PAD_TO_SIZE(dim->width, padding->width_padding);
+                scanline = PAD_TO_SIZE(dim->height, padding->height_padding);
+            }
 
             buf_planes->plane_info.frame_len =
-                    VENUS_BUFFER_SIZE(COLOR_FMT_NV21, dim->width, dim->height);
+                    VENUS_BUFFER_SIZE(COLOR_FMT_NV21, stride, scanline);
             buf_planes->plane_info.num_planes = 2;
             buf_planes->plane_info.mp[0].len = (uint32_t)(stride * scanline);
             buf_planes->plane_info.mp[0].offset = 0;
@@ -3916,8 +3932,13 @@ int32_t mm_stream_calc_offset_video(cam_format_t fmt,
             buf_planes->plane_info.mp[0].scanline = scanline;
             buf_planes->plane_info.mp[0].width = dim->width;
             buf_planes->plane_info.mp[0].height = dim->height;
-            stride = VENUS_UV_STRIDE(COLOR_FMT_NV21, dim->width);
-            scanline = VENUS_UV_SCANLINES(COLOR_FMT_NV21, dim->height);
+            if (stream_info->stream_type != CAM_STREAM_TYPE_OFFLINE_PROC) {
+               stride = VENUS_UV_STRIDE(COLOR_FMT_NV21, dim->width);
+               scanline = VENUS_UV_SCANLINES(COLOR_FMT_NV21, dim->height);
+            } else {
+               stride = PAD_TO_SIZE(dim->width, padding->width_padding);
+               scanline = PAD_TO_SIZE(dim->height, padding->height_padding);
+            }
             buf_planes->plane_info.mp[1].len =
                     buf_planes->plane_info.frame_len -
                     buf_planes->plane_info.mp[0].len;
@@ -3930,7 +3951,7 @@ int32_t mm_stream_calc_offset_video(cam_format_t fmt,
             buf_planes->plane_info.mp[1].height = dim->height / 2;
 #else
             LOGD("Video format VENUS is not supported = %d",
-                     fmt);
+                     stream_info->fmt);
 #endif
             break;
         case CAM_FORMAT_YUV_420_NV12_UBWC:
@@ -3980,12 +4001,12 @@ int32_t mm_stream_calc_offset_video(cam_format_t fmt,
 
 #else
             LOGD("Video format UBWC is not supported = %d",
-                     fmt);
+                     stream_info->fmt);
             rc = -1;
 #endif
             break;
         default:
-            LOGD("Invalid Video Format = %d", fmt);
+            LOGD("Invalid Video Format = %d", stream_info->fmt);
             rc = -1;
             break;
     }
@@ -4397,8 +4418,8 @@ int32_t mm_stream_calc_offset_postproc(cam_stream_info_t *stream_info,
                                             plns);
         break;
     case CAM_STREAM_TYPE_VIDEO:
-        rc = mm_stream_calc_offset_video(stream_info->fmt,
-                &stream_info->dim, plns);
+        rc = mm_stream_calc_offset_video(stream_info,
+                padding, plns);
         break;
     case CAM_STREAM_TYPE_RAW:
         rc = mm_stream_calc_offset_raw(stream_info->fmt,
@@ -4525,8 +4546,8 @@ int32_t mm_stream_calc_offset(mm_stream_t *my_obj)
                                             &my_obj->stream_info->buf_planes);
         break;
     case CAM_STREAM_TYPE_VIDEO:
-        rc = mm_stream_calc_offset_video(my_obj->stream_info->fmt,
-                &dim, &my_obj->stream_info->buf_planes);
+        rc = mm_stream_calc_offset_video(my_obj->stream_info,
+                &my_obj->padding_info, &my_obj->stream_info->buf_planes);
         break;
     case CAM_STREAM_TYPE_RAW:
         rc = mm_stream_calc_offset_raw(my_obj->stream_info->fmt,