c6cc9693d7d19fb365d0aec7067622c1cf18e676
Siddhesh Haldikar <120026104+thebassraja@users.noreply.github.com>
2022-12-17 17:16:26 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_vince.mk
+    $(LOCAL_DIR)/evolution-x_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_vince-user \
-    spark_vince-userdebug \
-    spark_vince-eng
+    evolution-x_vince-user \
+    evolution-x_vince-userdebug \
+    evolution-x_vince-eng
